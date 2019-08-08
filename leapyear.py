a76e66f2a7f5c7ebf0bca3ea616dd3e263d2f952
yogaammu <37235816+yogaammu@users.noreply.github.com>
2019-08-08 14:12:49 +0530

    Update leapyear.py

--- leapyear.py
+++ leapyear.py
@@ -1,9 +1,9 @@
 year = int(input())
 if year % 4 == 0 and year % 100 != 0:
-    print("Yes")
+    print("yes")
 elif year % 100 == 0:
-    print("No")
+    print("no")
 elif year % 400 == 0:
-    print("Yes")
+    print("yes")
 else:
-    print("No")
+    print("no")
