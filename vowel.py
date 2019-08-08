6620ecfd3c62eca4b713a0f29288c08a1275026f
yogaammu <37235816+yogaammu@users.noreply.github.com>
2019-08-08 12:00:10 +0530

    Update vowel.py

--- vowel.py
+++ vowel.py
@@ -1,5 +1,7 @@
-char = input("Enter a Character : ")
+char = input()
 if(char=='A' or char=='a' or char=='E' or char=='e' or char=='I' or char=='i' or char=='O' or char=='o' or char=='U' or char=='u'):
-    print(char,"is a vowel")
+    print("Vowel")
+elif(char=='!' or char=='@' or char=='#' or char=='$' or char=='%' or char=='^' or char=='&' or char=='*' or char=='~'):
+    print("Invalid")
 else:
-    print(char,"is a constant")
+    print("Consonant")
